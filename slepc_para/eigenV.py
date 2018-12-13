3fdabb6ff4cdea909b4981bae162d084973c12f4
wenya-r <wwrowe@gmail.com>
2018-12-13 11:07:18 -0500

    adding number of eigenvalues

--- slepc_para/eigenV.py
+++ slepc_para/eigenV.py
@@ -7,26 +7,30 @@ import numpy
 import time
 
 #@profile
-def eigenc(data, indices, indptr, Ns):
-
+def eigenc(data, indices, indptr, Ns, numE=1):
     opts = PETSc.Options()
     n = opts.getInt('n', Ns)
 
     _time2 = time.time()
     A = PETSc.Mat(); #A.create()
-    A.createAIJ(size = [n, n], csr=(indptr, indices, data) )
+    A.createAIJ(size = [n, n], csr=(indptr, indices, data), comm=PETSc.COMM_WORLD )
+# test
+
+
+
+#
     #A.setSizes([n, n])
     A.setFromOptions()
     A.setUp()
 
-    print('time for loop =',time.time()-_time2, 'secs')
+#    print('time for loop =',time.time()-_time2, 'secs')
     A.assemble()
 
     E = SLEPc.EPS(); E.create()
 
     E.setOperators(A)
     E.setProblemType(SLEPc.EPS.ProblemType.HEP)
-
+    E.setDimensions(nev=numE)
 
 
     history = []
@@ -62,7 +66,7 @@ def eigenc(data, indices, indptr, Ns):
 
     nconv = E.getConverged()
     Print( "Number of converged eigenpairs %d" % nconv )
-
+    Eigenvalues = []
     if nconv > 0:
 	  # Create the results vectors
           vr, vi = A.createVecs()
@@ -77,4 +81,5 @@ def eigenc(data, indices, indptr, Ns):
               Print( " %9f%+9f j %12g" % (k.real, k.imag, error) )
             else:
               Print( " %12f       %12g" % (k.real, error) )
-    return k
+            Eigenvalues.append(k.real)
+    return Eigenvalues
